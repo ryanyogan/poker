49eede1fd92ce9d2549bb9e3339c04a7e14b00eb
ryan <ryanyogan@gmail.com>
2021-05-24 20:23:15 -0500

    implement our own seeker based on the primitive base type io.Writer

--- cmd/api/file_system_store.go
+++ cmd/api/file_system_store.go
@@ -6,15 +6,16 @@ import (
 )
 
 type FileSystemPlayerStore struct {
-	database io.ReadWriteSeeker
+	database io.Writer
 	league   League
 }
 
 func NewFileSystemPlayerStore(database io.ReadWriteSeeker) *FileSystemPlayerStore {
 	database.Seek(0, 0)
 	league, _ := NewLeague(database)
+
 	return &FileSystemPlayerStore{
-		database: database,
+		database: &tape{database},
 		league:   league,
 	}
 }
@@ -42,6 +43,5 @@ func (f *FileSystemPlayerStore) RecordWin(name string) {
 		f.league = append(f.league, Player{name, 1})
 	}
 
-	f.database.Seek(0, 0)
 	json.NewEncoder(f.database).Encode(f.league)
 }
