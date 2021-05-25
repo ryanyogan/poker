4d7035fdc4494a0d64d0858fd3f5cddfd343e7f2
ryan <ryanyogan@gmail.com>
2021-05-25 14:09:48 -0500

    abstract away opening the (db) file

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
 
 	"yogan.dev/poker"
 )
@@ -11,15 +10,12 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
-	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
-	}
+	store, close, err := poker.FileSystemPlayerStoreFromFile(dbFileName)
 
-	store, err := poker.NewFileSystemPlayerStore(db)
 	if err != nil {
-		log.Fatalf("problem creating file system player store, %v", err)
+		log.Fatal(err)
 	}
+	defer close()
 
 	server := poker.NewPlayerServer(store)
 
